a69c8ca1f5950960ef80f4d6d60a6f36a6d9ac0a
NeoRazorX <neorazorx@gmail.com>
2015-04-18 18:21:02 +0200

    - Añadida comprobación de permisos. - Añadidos parámetros.

--- functions.php
+++ functions.php
@@ -1,9 +1,8 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Carlos García Gómez      neorazorx@gmail.com
+ * @copyright 2015, Carlos García Gómez. All Rights Reserved. 
  */
 
 /**
@@ -13,7 +12,7 @@
  * @param type $jrxml_location
  * @return string
  */
-function fs_jasper($jrxml_location)
+function fs_jasper($jrxml_location, $params = FALSE)
 {
    $salida = FALSE;
    
@@ -24,7 +23,11 @@ function fs_jasper($jrxml_location)
    }
    
    $cdir = getcwd();
-   if( file_exists($cdir.'/'.$jrxml_location) )
+   if( !is_executable($cdir.'/plugins/jasper/jasperstarter/bin/'.$bin) )
+   {
+      echo $cdir.'/plugins/jasper/jasperstarter/bin/'.$bin.' no tiene permisos de ejecución.';
+   }
+   else if( file_exists($cdir.'/'.$jrxml_location) )
    {
       /// nos movemos al directorio del report
       if( chdir( dirname($cdir.'/'.$jrxml_location) ) )
@@ -51,6 +54,16 @@ function fs_jasper($jrxml_location)
             /// generamos el PDF
             $cmd = $cdir."/plugins/jasper/jasperstarter/bin/".$bin." pr ".$jasper_location." -t ".$dbtype.
                     " -u ".FS_DB_USER." -p ".FS_DB_PASS." -o ".$pdf_name." -f pdf -H ".FS_DB_HOST." -n ".FS_DB_NAME;
+            
+            if($params)
+            {
+               $cmd .= ' -P';
+               foreach($params as $key => $value)
+               {
+                  $cmd .= ' '.$key.'='.$value;
+               }
+            }
+            
             exec($cmd);
             
             if( file_exists($pdf_name.'.pdf') )
@@ -63,6 +76,8 @@ function fs_jasper($jrxml_location)
                rename($pdf_name.'.pdf', $cdir.'/tmp/jasper/'.$pdf_name.'.pdf');
                $salida = 'tmp/jasper/'.$pdf_name.'.pdf';
             }
+            else
+               echo $cmd;
          }
          
          /// volvemos al directorio original
